a535dc2470117066becd12561c7821e532224bc6
Seth Weidman <sethweidman@fb.com>
2019-08-15 17:00:21 -0700

    Add second palindrom permutation solution

--- interview_questions/ctci/01_Chapter_1/04_palindrome_permutation.py
+++ interview_questions/ctci/01_Chapter_1/04_palindrome_permutation.py
@@ -1,4 +1,5 @@
 from string import ascii_lowercase
+from collections import Counter
 
 def palindrome_permutation(in_str: str) -> bool:
     '''
@@ -25,8 +26,29 @@ def palindrome_permutation(in_str: str) -> bool:
     return True
 
 
+def palindrome_permutation_counter(in_str: str) -> bool:
+    '''
+    Assumes in_str has only letters and spaces
+    '''
+    counter = Counter()
+    list_str = [char.lower() for char in in_str]
+    for char in list_str:
+        if char not in ascii_lowercase:
+            continue
+        else:
+            counter[char] += 1
+    
+    num_odd = 0
+    for count in counter.values():
+        if count % 2 == 1:
+            num_odd += 1
+        if num_odd > 1:
+            return False
+
+    return True
+
 if __name__=="__main__":
-    print(palindrome_permutation('Mr  John    Smith    '))
-    print(palindrome_permutation('abc'))
-    print(palindrome_permutation('aba')) 
-    print(palindrome_permutation('abA'))        
+    print(palindrome_permutation_counter('Mr  John    Smith    '))
+    print(palindrome_permutation_counter('abc'))
+    print(palindrome_permutation_counter('aba')) 
+    print(palindrome_permutation_counter('abA'))        
