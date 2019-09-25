0ec646c9a578f2a1449131426acc869a7769ed5a
Seth Weidman <sethweidman@fb.com>
2019-09-24 19:02:25 -0700

    Add linked list delete kth element pointer solution

--- 01_interview_questions/01_ctci/02_Chapter_2/02_delete_kth_from_end.py
+++ 01_interview_questions/01_ctci/02_Chapter_2/02_delete_kth_from_end.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from data_structures import Node, UnorderedList
 
 class Index:
@@ -20,6 +21,22 @@ def remove_kth_from_last(u: UnorderedList, k: int) -> Node:
     kth_to_last_node = kth_node_from_last(u.head, k, i)
     return u.remove(kth_to_last_node.get_data())
 
+def remove_kth_from_last_pointers(u: UnorderedList, k: int) -> UnorderedList:
+    p1 = u.head
+    p2 = u.head
+
+    for i in range(k):
+        if p1 is None:
+            return u
+        else:
+            p1 = p1.get_next()
+
+    while p1 is not None:
+        p1 = p1.get_next()
+        p2 = p2.get_next()
+
+    return u.remove(p2.get_data())
+
 if __name__ == '__main__':
     u = UnorderedList()
     u.add(3)
@@ -30,5 +47,9 @@ if __name__ == '__main__':
     print(u)
     u.add(5)
     print(u)
-    remove_kth_from_last(u, 2)
-    print(u)
+    u2 = deepcopy(u)
+    u3 = deepcopy(u)
+    remove_kth_from_last(u2, 2)
+    remove_kth_from_last_pointers(u3, 1)
+    print(u2)
+    print(u3)
