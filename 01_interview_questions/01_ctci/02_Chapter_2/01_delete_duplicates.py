48ab3119c3fef5be4712aa253455311c2b2905a2
Seth Weidman <sethhweidman@gmail.com>
2019-09-29 17:31:54 -0700

    Update Chapter 2 problem 1 solutions

--- 01_interview_questions/01_ctci/02_Chapter_2/01_delete_duplicates.py
+++ 01_interview_questions/01_ctci/02_Chapter_2/01_delete_duplicates.py
@@ -9,13 +9,28 @@ def delete_duplicates(u: UnorderedList) -> None:
     d = {}
     previous = Node()
     node = u.head
-    while node != None:
-        if node.get_data() in d: # will never happen for the first Node
+    while node is not None:
+        if node.get_data() in d:  # will never happen for the first Node
             previous.set_next(node.get_next())
         else:
             d[node.get_data()] = True
             previous = node
         node = node.get_next()
+
+
+def delete_duplicates_no_buffer(u: UnorderedList) -> None:
+    '''
+    Runs in O(N^2) time, but O(1) space
+    '''
+    current = u.head
+    while current is not None:
+        runner = current
+        while runner.get_next() is not None:
+            if runner.get_next().get_data() == current.get_data():
+                runner.set_next(runner.get_next().get_next())
+            else:
+                runner = runner.get_next()
+        current = current.get_next()
 
 
 if __name__ == '__main__':
@@ -30,5 +45,5 @@ if __name__ == '__main__':
     print(u)
     u.add(3)
     print(u)        
-    delete_duplicates(u)
+    delete_duplicates_no_buffer(u)
     print(u)
